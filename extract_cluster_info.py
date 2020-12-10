b7bdac539d14870a9c4801aee1ce46bfc3331fbe
jian626 <smallstone626@outlook.com>
2020-12-10 13:33:37 +0000

    fix entry name

--- extract_cluster_info.py
+++ extract_cluster_info.py
@@ -23,7 +23,7 @@ def get_cluster_info(cluster_info_file, origin_file, dest_file):
         for i in range(len(records)):
             row = records.iloc[i] 
             try:
-                temp_list.append(entry_cluster_mapping[row['Entery']])
+                temp_list.append(entry_cluster_mapping[row['Entry']])
             except:
                 temp_list.append('unknown')
         records['Cluster name'] = temp_list
