492804556aa591011060eab1011ac106b3aed13f
jian626 <smallstone626@outlook.com>
2020-12-20 15:09:45 +0000

    test original

--- framework/tools/sequence_sampling.py
+++ framework/tools/sequence_sampling.py
@@ -4,25 +4,20 @@ class SequenceGenerator(Sequence):
     def __init__(self, data_manager, batch_size):
         self.data_manager = data_manager
         self.batch_size = batch_size
-        self.pos = 0
         x, y = self.data_manager.get_training_data()
         self.len = int(np.floor(len(x) / self.batch_size))
 
     def __getitem__(self, index):
-        print('----------------------__getitem__-----------:', index)
         x, y = self.data_manager.get_training_data()
         task_num = self.data_manager.get_task_num()
-        pos = self.pos
-        self.pos += 1
-        rx = x[index: index + self.batch_size] 
+        rx = x[index * self.batch_size : (index + 1)+ self.batch_size] 
         ry = []
         for i in range(task_num):
-            ry.append(y[i][index:index+self.batch_size])
+            ry.append(y[i][index * self.batch_size : (index+1) * self.batch_size])
         return rx, ry
 
     def __len__(self):
-        print('-----------------------__len__-------------:', self.len)
         return self.len
 
     def on_epoch_end(self):
-        print('---------------------on_epoch_end------------')
+        pass
