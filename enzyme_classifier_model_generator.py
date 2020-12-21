e68f026fc12ef82328ce7c895fc112f3f093836d
jian626 <smallstone626@outlook.com>
2020-12-21 17:11:12 +0000

    test

--- enzyme_classifier_model_generator.py
+++ enzyme_classifier_model_generator.py
@@ -47,11 +47,11 @@ def run(file_path=None, input_data_config={}, input_model_config={}, input_evalu
     data_config['class_example_threshhold'] = 1 
     #save data, if it is set, the data will be saved to files, the last one is the config file name, which save
     #object of python, and it will be saved with ending of .pkl
-    #data_config['save_data'] = ['train.tab', 'test.tab', 'data_config'] 
+    data_config['save_data'] = ['train.tab', 'test.tab', 'data_config'] 
 
     #reuse:to reused data in files as training and testing
     #the last name is the config file name without .pkl
-    data_config['reuse_data'] = ['train.tab', 'test.tab', 'data_config']
+    #data_config['reuse_data'] = ['train.tab', 'test.tab', 'data_config']
     
     model_config = {}
     #embedding dimension
