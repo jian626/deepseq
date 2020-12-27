4fb73040db0efcdb1435a030341bfc673034e6c7
Jian Wang <smallstone626@qq.com>
2020-12-27 05:09:10 +0000

    good

--- framework/tools/sequence_sampling.py
+++ framework/tools/sequence_sampling.py
@@ -22,34 +22,48 @@ class SequenceGenerator(Sequence):
                 self.cluster_info_store[cluster_name] = []
             cluster_members = self.cluster_info_store[cluster_name]
             cluster_members.append(i)
+        self.cluster_keys = None 
 
         self.reset()
 
     def reset(self):
         self.sample_len = self.sample_len_store
         self.cluster_info = copy.deepcopy(self.cluster_info_store)
+        for k in self.cluster_info:
+            random.shuffle(self.cluster_info[k])
+        self.cluster_keys = list(self.cluster_info.keys())
+        random.shuffle(self.cluster_keys)
+        self.cluster_keys = np.array(self.cluster_keys)
 
     def __getitem__(self, index):
         x, y = self.data_manager.get_training_data()
-        task_num = self.data_manager.get_task_num()
-        cluster_keys = list(self.cluster_info.keys())
         result = []
         sample_len = self.sample_len
         needed = min(self.batch_size, sample_len)
 
-        print(sample_len, needed, index)
+        print('get item begin:', sample_len, index, needed, len(self.cluster_keys), len(self.cluster_info))
 
-        if len(cluster_keys) >= needed:
+        need_to_del_keys_index  = []
+
+        if len(self.cluster_keys) >= needed:
             for i in range(needed):
-                key = cluster_keys[(i+(index * self.batch_size))% len(cluster_keys)]
-                members = self.cluster_info[key]
+                relative_index = (i+(index * self.batch_size))% len(self.cluster_keys)
+                key = self.cluster_keys[relative_index]
+                try:
+                    members = self.cluster_info[key]
+                except:
+                    print('---------bad key:', key)
+                    print(len(self.cluster_keys), len(self.cluster_info), i, len(need_to_del_keys_index))
+                    raise
                 result.append(members.pop()) 
                 sample_len -= 1
-                if not members:
+                if len(members) == 0:
+                    need_to_del_keys_index.append(relative_index)
                     del self.cluster_info[key]
         else:
             while needed>len(result):
                 cluster_keys = list(self.cluster_info.keys())
+                random.shuffle(cluster_keys)
                 for k in cluster_keys:
                     members = self.cluster_info[k]
                     result.append(members.pop())
@@ -58,18 +72,22 @@ class SequenceGenerator(Sequence):
                     sample_len -= 1
                     if needed <= len(result):
                         break
+        if need_to_del_keys_index:
+            print('need to del')
+            self.cluster_keys = np.delete(self.cluster_keys, need_to_del_keys_index)
 
         self.sample_len = sample_len
         training_set, _ = self.data_manager.get_training_and_test_set()
         rx = x[result] 
         ry = []
 
+        task_num = self.data_manager.get_task_num()
+
         for i in range(task_num):
             ry.append(y[i][result])
 
+        print('get item end:', index)
         return rx, ry
-
-
 
     def __len__(self):
         return self.batch_num
