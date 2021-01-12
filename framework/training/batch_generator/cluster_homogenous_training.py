33416dd138d64e2bb422169f8729ed78d0c5b425
jian626 <smallstone626@outlook.com>
2021-01-12 03:57:21 +0000

    got

--- framework/training/batch_generator/cluster_homogenous_training.py
+++ framework/training/batch_generator/cluster_homogenous_training.py
@@ -4,20 +4,7 @@ import random
 import copy
 class homogenous_cluster_training(cluster_training_base):
     def reset_samples(self):
-        train_examples = []
-        cluster_info = copy.deepcopy(self.cluster_info_store)
-        keys = list(self.cluster_info_store.keys())
-        random.shuffle(keys)
-        for k in keys:
-            random.shuffle(cluster_info[k])
-        index = 0
-        while keys:
-            i = index % len(keys)
-            k = keys[i]
-            info = cluster_info[k]
-            train_examples.append(info.pop())
-            if len(info) == 0:
-                del keys[i]
-                del cluster_info[k] 
-            index +=1
+        for _, v in self.cluster_info_store.items():
+            train_examples += v
+
         self.train_examples = train_examples
