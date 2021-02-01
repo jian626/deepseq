12ad26d88283bb022165d18135f5eef4182f9a98
jian626 <smallstone626@outlook.com>
2021-02-01 12:32:06 +0000

    add loss sampling simple version ok

---
+++ framework/algorithm/loss_function.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def binary_entropy_loss(y, y_, epsilon=1e-12):
+    y = np.clip(y, epsilon, 1. - epsilon)
+    y_ = np.clip(y_, epsilon, 1. - epsilon)
+    return -1 * (np.sum(y * np.log(y_) + (1-y) * np.log(1-y_), axis=1))
