57d2900e266b9e54725b80cc535b3452b285a989
jian626 <smallstone626@outlook.com>
2021-02-01 12:47:59 +0000

    add loss sampling simple version ok

--- framework/training/batch_generator/loss_sampling.py
+++ framework/training/batch_generator/loss_sampling.py
@@ -38,7 +38,7 @@ class loss_sampling(training_base):
         return True 
 
     def get_debug_file_name(self):
-        debug_file = utili.get_table_value(self.config,'debug_file', None)
+        debug_file = get_table_value(self.config,'debug_file', None)
         return debug_file 
 
     def reset_samples(self):
