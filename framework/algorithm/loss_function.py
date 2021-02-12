7d2d859025d1aaea9edbe31640b653a061e21624
jian626 <smallstone626@outlook.com>
2021-02-12 06:05:40 +0000

    replace self-made binary entropy function with a tensorflows

--- framework/algorithm/loss_function.py
+++ framework/algorithm/loss_function.py
@@ -5,7 +5,10 @@ from tensorflow.keras.losses import BinaryCrossentropy
 
 def binary_entropy(y, y_, epsilon=1e-12):
     bce = BinaryCrossentropy(from_logits=True)
-    return bce(y, y_).numpy()
+    res = []
+    for index in range(len(y)):
+        res.append(bce(y[index], y_[index]).numpy())
+    return np.array(res)
     #c_y_ = np.clip(y_, epsilon, 1. - epsilon)
     #one_minus_y_ = np.clip(1-y_, epsilon, 1. - epsilon)
     #return -1 * (np.sum(y * np.log(c_y_) + (1-y) * np.log(1-y_), axis=1))
