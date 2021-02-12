e7f1500d50c9c10e1d6a8e45ade7990ac1a90e50
jian626 <smallstone626@outlook.com>
2021-02-12 19:27:47 +0000

    back to mannal function for debug

--- framework/algorithm/loss_function.py
+++ framework/algorithm/loss_function.py
@@ -4,11 +4,7 @@ from tensorflow.keras.losses import BinaryCrossentropy
 
 
 
-def binary_entropy(y, y_, epsilon=1e-12):
-    res = np.zeros(len(y))
-    for index in range(len(y)):
-        res[index] = log_loss(y[index], y_[index]) 
-    return res
-    #c_y_ = np.clip(y_, epsilon, 1. - epsilon)
-    #one_minus_y_ = np.clip(1-y_, epsilon, 1. - epsilon)
-    #return -1 * (np.sum(y * np.log(c_y_) + (1-y) * np.log(1-y_), axis=1))
+def binary_entropy(y, y_, epsilon=1e-15):
+    c_y_ = np.clip(y_, epsilon, 1. - epsilon)
+    one_minus_y_ = np.clip(1-y_, epsilon, 1. - epsilon)
+    return -1 * (np.sum(y * np.log(c_y_) + (1-y) * np.log(one_minus_y_), axis=1))
